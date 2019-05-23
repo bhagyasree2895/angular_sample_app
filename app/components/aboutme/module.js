9e7969023212f1d63c9f69611d9761399735f01c
bhagyasree2895 <bhagyasree.c60@gmail.com>
2019-05-22 19:06:11 -0500

    committing after adding aboutme folder

---
+++ app/components/aboutme/module.js
@@ -0,0 +1,16 @@
+/*angular.module("myApp", [])
+      .controller("myFirstController",['$scope', function ($scope){
+          console.log("This is form controller");
+          $scope.hello="hello world";
+      }]);*/
+
+      angular.module("myApp").controller("myFirstController", function ($scope)
+      {
+          let Student= {
+              FirstName: "Bhagya Sree",
+              LastName : "Chanda",
+              StudentId : "S534662@nwmisouri.edu"
+          }
+          $scope.Student=Student;
+          
+      });
